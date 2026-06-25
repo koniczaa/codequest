71f431e318aeed2f81d8f5ac2d919cf9a6abf30d
konicza <kochamdissa15@gmail.com>
2026-06-25 09:20:36 +0200

    added reseting by 6 am

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 import json
+import datetime
 
 
 def menu():
-
+    czas = datetime.datetime.now()
     baza = {"Read Kana": False, "Solve Python Problem": False, "Learn 10 Words": False}
+    format = "%Y-%m-%d %H:%M:%S.%f"
     while True:
         try:
             with open("data.json", "r") as f:
                 baza = json.load(f)
+                date = datetime.datetime.strptime(baza["last save"], format)
         except (FileNotFoundError, json.JSONDecodeError):
             pass
-
+        if datetime.datetime.now().hour >= 6 and datetime.datetime.now().day > date.day:
+            for i in baza:
+                baza[i] = False
         for i in baza:
             if baza[i] == True:
                 print(f"[✔] {i}")
@@ -30,6 +35,9 @@ def menu():
         except ValueError:
             print("Enter a number")
             pass
+
+        baza["last save"] = str(datetime.datetime.today())
+
         zapis = json.dumps(baza, indent=3)
         with open("data.json", "w") as f:
             f.write(zapis)
